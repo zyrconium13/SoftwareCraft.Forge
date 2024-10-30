0ca81d7e04626d0d9bf01961f75d4158e3a0acf6
Eduard Popescu <peduard13@hotmail.com>
2024-10-30 19:06:12 +0200

    unit functional type

---
+++ SoftwareCraft.Forge/Unit.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftwareCraft.Functional
+{
+    public sealed class Unit
+    {
+        private static Unit? instance;
+
+        private Unit()
+        {
+        }
+
+        public static Unit Instance => instance ??= new Unit();
+    }
+}
